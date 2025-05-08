ae3d68d0d0b80a2023e91c32e6173894cb75a312
cdn-lgn <logan2246t@gmail.com>
2025-05-09 01:11:10 +0530

    nativewind configured

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+import './global.css';
+
+const App = () => {
+  return (
+    <View>
+      <Text className="text-4xl text-cyan-500">Hell No</Text>
+    </View>
+  );
+};
+export default App;
